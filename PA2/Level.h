65566f67bd402ca155457e0ce8a3d495b853771e
Josh Vaysman <vaysman@chapman.edu>
2024-09-11 18:18:36 -0700

    git commit

--- PA2/Level.h
+++ PA2/Level.h
@@ -7,18 +7,18 @@
 class Level
 {
 private:
-    int N;
-    char** grid;
-    void DeleteGrid(char** grid);
-    void MakeGrid(int n);
+    int N; // grid dimensions
+    char** grid; // Char array that Mario will traverse on his adventures
+    void DeleteGrid(char** grid); // Deletes grid
+    void MakeGrid(int n); // Makes a new grid, useful for new Level
 public:
-    Level();
-    Level(int n);
-    ~Level();
-    int GetN();
-    void SetN(int n);
-    char** GetGrid();
-    void SetGrid(int n);
+    Level(); // Def constructor
+    Level(int n); // Overloaded constructor
+    ~Level(); // Def Destructor
+    int GetN(); // Getter for N
+    void SetN(int n); // Setter for N
+    char** GetGrid(); // Getter for grid
+    void SetGrid(int n); // Setter for N
 };
 
 #endif
