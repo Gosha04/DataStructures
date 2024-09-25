177a20bd8cce64a66d44a59f3b43e9d97f03d4a3
Josh Vaysman <vaysman@chapman.edu>
2024-09-24 18:37:21 -0700

    git commit

--- Exercises/Queue/CQueue.h
+++ Exercises/Queue/CQueue.h
@@ -1,12 +1,12 @@
-#ifndef NQUEUE_H
-#define NQUEUE_H
+#ifndef CQUEUE_H
+#define CQUEUE_H
 
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
 template <typename T>
-class NQueue
+class CQueue
 {
 private:
     int m_count;   // Current number of elements in the queue
@@ -16,9 +16,9 @@ private:
     T* m_arr;      // Pointer to the array for storing queue elements
 
 public:
-    NQueue();           // Default constructor
-    NQueue(int size);   // Constructor with initial size
-    virtual ~NQueue();          // Destructor
+    CQueue();           // Default constructor
+    CQueue(int size);   // Constructor with initial size
+    virtual ~CQueue();          // Destructor
     void add(T value); // Enqueue (add element to the back)
     T remove();            // Dequeue (remove element from the front)
     T peek();           // Peek at the front element without removing
@@ -29,7 +29,7 @@ public:
 
 // Default constructor
 template <typename T>
-NQueue<T>::NQueue() { 
+CQueue<T>::CQueue() { 
     m_count = 0;
     m_max = 0;
     m_front = 0;
@@ -38,7 +38,7 @@ NQueue<T>::NQueue() {
 }
 
 template <typename T>
-NQueue<T>::NQueue(int size) {
+CQueue<T>::CQueue(int size) {
     m_count = 0;
     m_max = size;
     m_front = 0;
@@ -47,46 +47,49 @@ NQueue<T>::NQueue(int size) {
 }
 
 template <typename T>
-NQueue<T>::~NQueue() {
+CQueue<T>::~CQueue() {
     delete[] m_arr;
 }
 
 template <typename T>
-bool NQueue<T>::isEmpty() {
+bool CQueue<T>::isEmpty() {
     return (m_count == 0);
 }
 
 template <typename T>
-bool NQueue<T>::isFull() {
+bool CQueue<T>::isFull() {
     return (m_count == m_max);
 }
 
 template <typename T>
-int NQueue<T>::size() {
+int CQueue<T>::size() {
     return m_count;
 }
 
 template <typename T>
-T NQueue<T>::peek() {
+T CQueue<T>::peek() {
     return m_arr[m_front];
 }
 
 template <typename T>
-void NQueue<T>::add(T data) {
-    if (!isFull) { // space somewhere
+void CQueue<T>::add(T data) {
+    if (!((isFull()))) { // space somewhere
         if (m_back == m_max - 1) {
             m_back = 1;
         }
         m_arr[++m_back] = data;
         ++m_count;
     } else { // truly full
-        T* temp = new T[2 * max];
-        for (int i = 0; i < m_max; ++i) {
-            temp[i] = m_arr[i];
+        T* temp = new T[2 * m_max];
+        int tc = 0;
+        while (!isEmpty()) {
+            temp[tc++] = remove();
         }
         delete[] m_arr;
         m_arr = temp;
         m_max *= 2;
+        m_front = 0;
+        m_back = m_count;
         m_arr[++m_back] = data;
         ++m_count;
     }
@@ -107,5 +110,6 @@ T CQueue<T>::remove() {
         throw runtime_error("Queue is empty. Cannot remove element.");
     }
 }
+
 
 #endif
--- Exercises/Queue/example.cpp
+++ Exercises/Queue/example.cpp
@@ -1,11 +1,17 @@
-#include <iostream>
-
+#include "CQueue.h"
 int main(int argc, char const *argv[])
 {
-    for (int i = 1; i < 4; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            std::cout << j << std::endl;
-        }
+    CQueue<char> cq(4);
+    cq.add('e');
+    cq.add('r');
+    cq.add('i');
+    cq.add('k');
+    cout << cq.isFull() << endl;
+    cq.remove();
+    cq.add('l');
+    cq.add('i');
+    while (!cq.isEmpty()) {
+        cout << cq.remove() << endl;
     }
     return 0;
 }
