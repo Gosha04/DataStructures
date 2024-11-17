ee65fc76182d7e05097ede853d7ff57b4b4fd1b5
Josh Vaysman <vaysman@chapman.edu>
2024-11-17 11:31:59 -0800

    git commit

--- PA5/TournamentNode.cpp
+++ PA5/TournamentNode.cpp
@@ -1,4 +1,5 @@
 #include "TournamentNode.h"
+#include <iostream>
 
 TournamentNode::TournamentNode() {
     m_data = Monster();
@@ -18,12 +19,37 @@ TournamentNode::~TournamentNode() {
     
 }
 
+
+// seg faults if it is a by
+// don't think we need to worry about it as I assigned by's manually in TournamentTree
 Monster TournamentNode::fight() {
+    Monster loser;
     if (m_left-> m_data.screamFight(m_right -> m_data)) {
         m_winner = m_left;
+        loser = m_right -> m_data;
     } else {
         m_winner = m_right;
+        loser = m_left -> m_data;
     }
 
-    return m_winner -> m_data;
+    return loser;
+}
+
+int main(int argc, char const *argv[])
+{
+    Monster meh("Mike", 4);
+    Monster scarey("Sulley", 10);
+
+    TournamentNode* root = new TournamentNode();
+
+    TournamentNode* left = new TournamentNode(meh);
+    TournamentNode* right = new TournamentNode(scarey);
+
+    // switch this back to private after testing
+    root -> m_left = left;
+    root -> m_right = right;
+
+    std::cout << root->fight().getName() << std::endl;
+
+    return 0;
 }
--- PA5/TournamentNode.h
+++ PA5/TournamentNode.h
@@ -4,12 +4,14 @@ class TournamentNode
 {
 private:
     TournamentNode* m_winner;
-    TournamentNode* m_left;
-    TournamentNode* m_right;
+    // TournamentNode* m_left;
+    // TournamentNode* m_right;
     Monster m_data;
     bool m_hasData;
     friend class TournamentTree;
 public:
+    TournamentNode* m_left;
+    TournamentNode* m_right;
     TournamentNode();
     TournamentNode(Monster monster);
     virtual ~TournamentNode();
