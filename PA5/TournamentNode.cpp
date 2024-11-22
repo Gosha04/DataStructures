2d6168b0a8132817ee38d315054e6c7068872090
Divi Newton <divinewton@gmail.com>
2024-11-21 23:44:01 -0800

    git commit

--- PA5/TournamentNode.cpp
+++ PA5/TournamentNode.cpp
@@ -1,6 +1,17 @@
+/*
+AUTHOR INFO
+Names: Joshua Vaysman, Divi Newton
+Student IDs: 2449656, 2440117
+Emails: vaysman@chapman.edu, dnewton@chapman.edu
+Class Section: CPSC-350-02
+Assignment: PA5 - Scare Games
+*/
+
 #include "TournamentNode.h"
 #include <iostream>
 
+// default constructor
+// sets all variables to NULL
 TournamentNode::TournamentNode() {
     m_data = Monster();
     m_winner = NULL;
@@ -8,6 +19,8 @@ TournamentNode::TournamentNode() {
     m_right = NULL;
 }
 
+// overloaded constructor
+// sets variables to NULL and sets data to a specified Monster
 TournamentNode::TournamentNode(Monster monster) {
     m_data = monster;
     m_winner = NULL;
@@ -15,23 +28,23 @@ TournamentNode::TournamentNode(Monster monster) {
     m_right = NULL;
 }
 
+// destructor
 TournamentNode::~TournamentNode() {
     
 }
 
-
-// seg faults if it is a by
-// don't think we need to worry about it as I assigned by's manually in TournamentTree
+// fight method returns the loser based on two monsters fighting each other
+// contains all nessesary logic for fighting monsters in a node
 Monster TournamentNode::fight() {
     Monster loser;
     std::cout << "Fighting: " << m_left->m_data.getName() << " vs " << m_right->m_data.getName() << std::endl;
-    if (m_left -> m_data.getScream() == m_right -> m_data.getScream()) {
+    if (m_left -> m_data.getScream() == m_right -> m_data.getScream()) { // if the screams are equal
         m_winner = m_left;
         setData(m_winner->m_data);
         loser = m_right->m_data;
         return loser;
     }
-    if (m_right -> m_hasData) {
+    if (m_right -> m_hasData) { // if the right side has data
         if (m_left->m_data.screamFight(m_right->m_data)) {
             m_winner = m_left;
             setData(m_winner->m_data);
@@ -40,7 +53,7 @@ Monster TournamentNode::fight() {
             m_winner = m_right;
             loser = m_left->m_data;
         }   
-    } else {
+    } else { // if right side has no data
         if (m_right == nullptr) {
             m_winner = m_left;
             setData(m_winner -> m_data);
@@ -51,51 +64,12 @@ Monster TournamentNode::fight() {
         loser = m_right -> m_data;
     }
     std::cout << "Loser: " << loser.getName() << std::endl;
-    return loser;
+    return loser; // returns the looser
 }
 
 
-
+// setData method sets the data of a node (the winner) to a specific monster
+// used from fight method to define the winner in a TournamentNode object
 void TournamentNode::setData(Monster monster) {
     m_data = monster;
 }
-
-// int main(int argc, char const *argv[])
-// {
-//     Monster meh("Mike", 4);
-//     Monster A("A", 1);
-//     Monster scarey("Sulley", 10);
-//     Monster B("B", 0);
-
-//     TournamentNode* root = new TournamentNode();
-//     TournamentNode* rl = new TournamentNode();
-//     TournamentNode* rr = new TournamentNode();
-
-
-
-//     TournamentNode* l1 = new TournamentNode(meh);
-//     TournamentNode* l2 = new TournamentNode(A);
-//     TournamentNode* r1 = new TournamentNode(scarey);
-//     TournamentNode* r2 = new TournamentNode(B);
-
-
-//     // switch this back to private after testing
-//     root -> m_left = rl;
-//     root -> m_right = rr;
-
-//     rl -> m_left = l1;
-//     rl -> m_right = l2;
-
-//     rr -> m_left = r1;
-//     rr -> m_right = r2;
-
-
-//     rl->fight();
-//     // rl->setData(rl -> m_winner -> m_data);
-//     rr->fight();
-//     // rr->setData(rr -> m_winner -> m_data);
-//     root -> fight();
-
-
-//     return 0;
-// }
